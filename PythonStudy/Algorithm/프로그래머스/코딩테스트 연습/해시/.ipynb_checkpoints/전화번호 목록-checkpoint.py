b66c0498a51769abac6634e19a21bdcc2d8038dc
RANGSHOW <rangshow0306@gmail.com>
2021-07-22 22:58:17 +0900

    전화번호 목록-2.py

--- PythonStudy/Algorithm/프로그래머스/코딩테스트 연습/해시/.ipynb_checkpoints/전화번호 목록-checkpoint.py
+++ PythonStudy/Algorithm/프로그래머스/코딩테스트 연습/해시/.ipynb_checkpoints/전화번호 목록-checkpoint.py
@@ -4,6 +4,9 @@ phone_book = ["12","123","1235","567","88"]
 answer = False
 
 def solution(phone_book):
-    
-    
+    answer = True
+    for i in range(len(phone_book)):
+        for j in range(i + 1, len(phone_book)):
+            if phone_book[i] in phone_book[j]:
+                answer = False
     return answer
