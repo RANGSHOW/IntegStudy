bf9608e7f91f1dd0a683f3c79806641fc0dd7334
RANGSHOW <rangshow0306@gmail.com>
2021-07-22 22:25:46 +0900

    전화번호 목록.py

---
+++ PythonStudy/Algorithm/프로그래머스/코딩테스트 연습/해시/전화번호 목록.py
@@ -0,0 +1,9 @@
+# 접두사인 경우 false, 접두사가 아니면 true
+
+phone_book = ["12","123","1235","567","88"]
+answer = False
+
+def solution(phone_book):
+    
+    
+    return answer
--- PythonStudy/pythonTest.py
+++ PythonStudy/pythonTest.py
@@ -209,3 +209,4 @@ myList = [1, 2, 3, 4, 5]
 
 myList.append(6)
 print(myList)
+
