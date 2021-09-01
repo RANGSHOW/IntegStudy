5395db05f3322c0c7523c4cc7af348069e242046
RANGSHOW <rangshow0306@gmail.com>
2021-09-01 22:25:53 +0900

    2021.09.01

--- PythonStudy/pythonTest.py
+++ PythonStudy/pythonTest.py
@@ -211,3 +211,9 @@ myList = [1, 2, 3, 4, 5]
 myList.append(6)
 
 
+def bubble_sort(arr):
+    for i in range(0, len(arr)):
+        for j in range(i, len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j + 1], arr[j] = arr[j], arr[j + 1]
+
