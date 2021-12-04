a300c5525a63396f8928e509d8cf5631cfb0e0f9
RANGSHOW <rangshow0306@gmail.com>
2021-12-04 21:46:17 +0900

    2021.12.04

---
+++ PythonStudy/Machine Learning/freenote.py
@@ -0,0 +1,11 @@
+# 리스트에서 숫자의 개수를 세어주는 프로그램을 만들어보자 (collections의 Counter)
+
+def my_counter(L):
+    return "Hello"
+
+if __name__ == "__main__":
+    return_value = my_counter([1, 2, 3])
+    print(return_value)
+
+
+
