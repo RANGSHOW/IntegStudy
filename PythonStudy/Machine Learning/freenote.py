b2f2dadc868404fbc6dcc316e838469a634880bd
RANGSHOW <rangshow0306@gmail.com>
2021-12-06 14:56:41 +0900

    2021.12.06

--- PythonStudy/Machine Learning/freenote.py
+++ PythonStudy/Machine Learning/freenote.py
@@ -1,11 +1,7 @@
-# 리스트에서 숫자의 개수를 세어주는 프로그램을 만들어보자 (collections의 Counter)
+for _ in range(10):
+    print("Hello World!")
 
-def my_counter(L):
-    return "Hello"
+L = [str(x) for x in range(10)]
+print(L)
 
-if __name__ == "__main__":
-    return_value = my_counter([1, 2, 3])
-    print(return_value)
-
-
-
+def my_sum(asdf)
