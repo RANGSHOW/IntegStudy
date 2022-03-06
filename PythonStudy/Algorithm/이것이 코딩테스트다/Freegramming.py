fbb9f0f66c433c3063b2a0488bde4cb1a4ae3422
RANGSHOW <rangshow0306@gmail.com>
2022-03-06 18:23:09 +0900

    2022.03.06

---
+++ PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
@@ -0,0 +1,12 @@
+from logging import root
+
+import pandas as pd
+import random as rd
+
+root_num = 20
+for i in range(root_num):
+    print("{} 번째 시도".format(i))
+    rd.seed(i)
+    for j in range(root_num):
+        pass
+
