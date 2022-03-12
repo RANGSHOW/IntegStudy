adc62896916e206d47b9bf64c81b65400307a77c
RANGSHOW <rangshow0306@gmail.com>
2022-03-13 03:41:26 +0900

    2022.03.13

--- PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
+++ PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
@@ -1,14 +1,15 @@
-from logging import root
-from xml.dom import NoModificationAllowedErr
-
-import pandas as pd
 import random as rd
 
-root_num = 20
-for i in range(root_num):
-    print("{} 번째 시도".format(i))
-    rd.seed(i)
-    for j in range(root_num):
-        pass
+for i in range(10):
+    for j in range(10):
+        print("{} X {} = {}".format(i, j, i * j))
+    print()
 
-    
+
+import random as rd
+import pandas as pd
+import numpy as np
+
+my_string = 'abcde'
+my_arr = np.asarray(my_string)
+print(my_arr)
