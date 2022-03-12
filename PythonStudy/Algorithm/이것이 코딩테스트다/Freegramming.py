17bac029c044d50e218bbeb6bbdefaf79991455b
RANGSHOW <rangshow0306@gmail.com>
2022-03-12 16:27:52 +0900

    2022.03.12

--- PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
+++ PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
@@ -11,30 +11,4 @@ for i in range(root_num):
     for j in range(root_num):
         pass
 
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-my_figure = plt.figure((10, 10))
-plt.show(my_figure)
-
-
-
-for i in range(10):
-    for j in range(10):
-        print("{} X {} = {}".format(i, j, i * j))
-
-import random as rd
-my_arr = [1, 2, 3, 4, 5]
-rd.shuffle(my_arr)
-print(my_arr)
-
-class Person():
-    def __init__(self):
-        self.name = None
-me = Person()
-print(me.name())
-
-import 
-
+    
