3af578a224afe3cc8058e1cc4996416ef1ffc887
RANGSHOW <rangshow0306@gmail.com>
2022-03-14 23:19:56 +0900

    2022.03.14

--- PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
+++ PythonStudy/Algorithm/이것이 코딩테스트다/Freegramming.py
@@ -1,4 +1,5 @@
 import random as rd
+from unicodedata import name
 
 for i in range(10):
     for j in range(10):
@@ -13,3 +14,26 @@ import numpy as np
 my_string = 'abcde'
 my_arr = np.asarray(my_string)
 print(my_arr)
+
+for i in range(100):
+    for j in range(100):
+        print("{} X {} = {}".format(i, j, i * j))
+    print()
+    
+
+class Person():
+    def __init__(self):
+        self.name = None
+    
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+        
+    def set_age(self, age):
+        self.age = age
+
+    def get_age(self):
+        return self.age
+
