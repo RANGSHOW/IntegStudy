a279d7a188012c76ac578a531a6e9f61668ed229
RANGSHOW <rangshow0306@gmail.com>
2022-03-25 03:15:26 +0900

    2022.03.25

--- PythonStudy/Freegramming.py
+++ PythonStudy/Freegramming.py
@@ -1,1 +1,12 @@
-print(2 + 2)
+class Person():
+    def __init__(self):
+        self.name = None
+    def set_name(self, name):
+        self.name = name
+    def get_name(self):
+        return self.name
+
+me = Person()
+print(me.get_name())
+me.set_name("ChangHyeon")
+print(me.get_name())
