d45b251356d7e375acd5d9422c74f7a507afe49c
RANGSHOW <rangshow0306@gmail.com>
2022-06-30 22:52:41 +0900

    2022.06.30

---
+++ exerciseManager/src/test/java/com/multi/exercise/ExerciseManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.multi.exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.multi.management.ExerciseManagerApplication;
+
+@SpringBootTest(classes = ExerciseManagerApplication.class)
+class ExerciseManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
