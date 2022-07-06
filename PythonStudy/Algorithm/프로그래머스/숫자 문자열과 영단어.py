ce0a81ef599e296b93ca728cef17dfc50e39d90b
RANGSHOW <rangshow0306@gmail.com>
2022-07-06 20:24:52 +0900

    2022.07.06

---
+++ PythonStudy/Algorithm/프로그래머스/숫자 문자열과 영단어.py
@@ -0,0 +1,21 @@
+"""
+s	result
+"one4seveneight"	1478
+"23four5six7"	234567
+"2three45sixseven"	234567
+"123"	123
+
+
+숫자	영단어
+0	zero
+1	one
+2	two
+3	three
+4	four
+5	five
+6	six
+7	seven
+8	eight
+9	nine
+"""
+
