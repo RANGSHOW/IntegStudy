dd70a3aa84bccdb1e01f1188bf9b54e1a03492bd
RANGSHOW <rangshow0306@gmail.com>
2022-07-19 21:51:24 +0900

    2022.07.19

--- PythonStudy/Algorithm/프로그래머스/신고 결과 받기.py
+++ PythonStudy/Algorithm/프로그래머스/신고 결과 받기.py
@@ -4,19 +4,15 @@
 
 def solution(id_list, report, k):
     report = list(set(report))  # 동일 신고 제거
+    # 이중 for문 안쓰고 하는 법 생각해보기
     for id in id_list:
         temp = 0
-        
         for reporter, reportee in report:
             if id == reportee:
-                temp
+                temp += 1
+                
     answer = []
     return answer
 
 
 aList = ["ryan con", "ryan con", "ryan con", "ryan con"]
-
-aList = set(aList)
-print(aList)
-aList = list(aList)
-print(aList)
