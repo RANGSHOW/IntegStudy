83e4c4f6d25d0cc33c2aac5088399134a8c0a6d2
RANGSHOW <rangshow0306@gmail.com>
2022-07-21 23:59:19 +0900

    2022.07.21

--- PythonStudy/Algorithm/프로그래머스/신고 결과 받기.py
+++ PythonStudy/Algorithm/프로그래머스/신고 결과 받기.py
@@ -2,17 +2,21 @@
 # ["muzi", "frodo", "apeach", "neo"]	["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]	2	[2,1,1,0]
 # ["con", "ryan"]	["ryan con", "ryan con", "ryan con", "ryan con"]	3	[0,0]
 
-def solution(id_list, report, k):
+def solution(id_list, report):
     report = list(set(report))  # 동일 신고 제거
     # 이중 for문 안쓰고 하는 법 생각해보기
-    for id in id_list:
+    for i in range(len(id_list)):
+        answer = [0] * len(id_list)
         temp = 0
-        for reporter, reportee in report:
-            if id == reportee:
+        for single_repo in report:
+            print((single_repo.split(" "))[1])
+            if id_list[i] == (single_repo.split(" "))[1]:
                 temp += 1
-                
-    answer = []
+        answer[i] = temp
     return answer
 
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
 
-aList = ["ryan con", "ryan con", "ryan con", "ryan con"]
+print(solution(id_list, report))
+print(solution(id_list, report))
