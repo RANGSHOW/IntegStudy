088971b2d292cceda9f191bbcf71c3b43569ec79
RANGSHOW <rangshow0306@gmail.com>
2023-01-12 22:01:00 +0900

    2022.01.12

--- SushiClassifier/images/codes/code.py
+++ SushiClassifier/images/codes/code.py
@@ -1,6 +1,11 @@
+import random as rd
+import pandas as pd
+import numpy as np
+
 def sum(num1: int, num2: int) -> int:
     pass
 
 if __name__ == "__main__":
     print("Hell World!")
     print("df")
+    
